8c55c17d533c7a0a007dcd195ecb9d8194ed5092
André Moura <andre.benedicto@etec.sp.gov.br>
2021-06-20 16:57:11 -0300

    tela login

--- src/businessschedule/BusinessSchedule.java
+++ src/businessschedule/BusinessSchedule.java
@@ -1,85 +1,15 @@
 package businessschedule;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import businessschedule.apresentacao.FrmHome;
+import businessschedule.apresentacao.FrmLogin;
 
-import businessschedule.modelo.classes.Escala;
-import java.util.Scanner;
-import businessschedule.modelo.dao.AdministradorDAO;
-import businessschedule.modelo.classes.Funcionario;
-import businessschedule.modelo.classes.Usuario;
-import businessschedule.modelo.classes.FuncionarioHorario;
-import businessschedule.modelo.classes.Programa;
-import businessschedule.modelo.classes.Switcher;
-import businessschedule.modelo.classes.Estudio;
-//import classes.dao.EscalaDAO;
-//import classes.dao.ProgramaDAO;
-import businessschedule.modelo.dao.SwitcherDAO;
-import businessschedule.modelo.dao.EstudioDAO;
-import businessschedule.modelo.dao.FuncionarioDAO;
-//import classes.dao.FuncionarioHorarioDAO;
-
-/**
- *
- * @author moura
- */
 public class BusinessSchedule {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        /*FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-        //Funcionario funcionario = new Funcionario(4, "Fulano de Tal", "fulano@teste.com", "fulano", "Op Camera");
+        FrmLogin login = new FrmLogin();
+        login.setVisible(true);
         
-        for (Funcionario funcionario : funcionarioDAO.listar()) {
-            System.out.println(funcionario);
-        }*/
-        //funcionarioDAO.alterar(funcionario);
-        //System.out.println(funcionarioDAO.buscar(4));
-        
-        //funcionarioDAO.excluir(funcionario);
-        //funcionarioDAO.inserir(funcionario);
-        //System.out.println( funcionarioDAO.lastId());
-        
-          /* ProgramaDAO programaDAO = new ProgramaDAO();
-
-             for (Programa programa : programaDAO.listar()) {
-            System.out.println(programa);
-        
-        
-        System.out.println("\n" + programaDAO.buscar(1));
-            } */
-
-        /* long data = new Date().getTime();
-        DateFormat dataFormatada = new SimpleDateFormat("HH:mm:ss");
-        System.out.println(dataFormatada.format(data));
-        System.out.println(dataFormatada.format(data + 60 * 60)); */
-        //System.out.println(new Date());
-        /*Switcher switcher = new Switcher(1, "Switcher 1");
-        Estudio estudio = new Estudio(1, "Estudio 1");
-        Programa programa = new Programa(1, "Saude e Fe", "7:00:00", "8:00:00", "tipo", "2021-06-18", switcher, estudio);
-        System.out.println(programa.getData());*/
-        
-        /*Scanner leia = new Scanner(System.in);
-        String nome;
-        
-        try{
-                
-             Funcionario funcionario = new Funcionario(1, "rerer","ererer", "4ttrtrtter", "reref");
-             System.out.println(funcionario);
-            
-        }catch (Exception e){
-            
-            System.out.println(e.getMessage());
-            
-        }*/
-        
-        /*AdministradorDAO funcionarioDAO = new AdministradorDAO();*/
-        
-        FuncionarioDAO funcionarioDAO = new FuncionarioDAO();
-        
-        funcionarioDAO.buscarNomeFuncionario("a");
+        //FrmHome home = new FrmHome();
+        //home.setVisible(true);
     }
 }
---
+++ src/businessschedule/apresentacao/FrmHome.java
@@ -0,0 +1,164 @@
+package businessschedule.apresentacao;
+
+import java.awt.Dimension;
+import javax.swing.GroupLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.LayoutStyle;
+import javax.swing.WindowConstants;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.Font;
+import java.awt.Toolkit;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class FrmHome extends JFrame {
+    private JButton btnCadastrar;
+    private JButton btnEditar;
+    private JButton btnSair;
+    private JLabel jLabel1;
+    private JLabel jLabel2;
+    private JScrollPane jScrollPane1;
+    private JTable table;
+
+    public FrmHome() {
+        super("Business Schedule");
+        initComponents();
+    }
+
+    private void initComponents() {
+        jScrollPane1 = new JScrollPane();
+        table = new JTable();
+        jLabel1 = new JLabel();
+        btnCadastrar = new JButton();
+        btnEditar = new JButton();
+        btnSair = new JButton();
+        jLabel2 = new JLabel();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        table.setModel(new DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(table);
+
+        jLabel1.setIcon(new ImageIcon(getClass().getResource("/businessschedule/apresentacao/img/logo.png")));
+
+        btnCadastrar.setText("Cadastrar");
+        btnCadastrar.addActionListener(new CadastrarListener());
+
+        btnEditar.setText("Editar");
+        btnEditar.addActionListener(new EditarListener());
+
+        btnSair.setText("Sair");
+        btnSair.addActionListener(new SairListener());
+
+        jLabel2.setFont(new Font("Segoe UI", 0, 48));
+        jLabel2.setText("Escala semanal");
+
+        GroupLayout layout = new GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 670, GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(181, 181, 181)
+                        .addComponent(btnCadastrar, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnSair, GroupLayout.PREFERRED_SIZE, 94, GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(layout.createSequentialGroup()
+                .addGap(35, 35, 35)
+                .addComponent(jLabel1, GroupLayout.PREFERRED_SIZE, 152, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(85, 85, 85))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(47, 47, 47)
+                        .addComponent(jLabel2))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addComponent(jLabel1, GroupLayout.PREFERRED_SIZE, 144, GroupLayout.PREFERRED_SIZE)))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 250, Short.MAX_VALUE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnEditar, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnCadastrar, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnSair, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18))
+        );
+
+        pack();
+        centralizar();
+    }
+
+    private void centralizar() {
+        Dimension resolucao = Toolkit.getDefaultToolkit().getScreenSize();
+        Dimension tamanhoTela = getSize();
+        
+        int largura = (resolucao.width - tamanhoTela.width) / 2;
+        int altura = (resolucao.height - tamanhoTela.height) / 2;
+        
+        setLocation(largura, altura);
+    }
+
+    private class CadastrarListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO add your handling code here:
+            /* OpCadastro ini = new OpCadastro();
+            ini.setVisible(true);
+            setVisible(false); */
+        }
+
+    }
+
+    private class EditarListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO add your handling code here:
+            /* OpEdicao ini = new OpEdicao();
+            ini.setVisible(true);
+            setVisible(false); */
+        }
+    }
+
+    private class SairListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // TODO add your handling code here:
+            if (JOptionPane.showConfirmDialog(null, "Deseja realmente sair?", "Mensagem do Programa", JOptionPane.YES_NO_OPTION) == 0) {
+                System.exit(0);
+            }
+        }
+    }
+}
