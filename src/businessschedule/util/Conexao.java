d833db9b92949d9b81fdce67a3b6312692596335
André Moura <andre.benedicto@etec.sp.gov.br>
2021-06-24 10:42:38 -0300

    gerar executavel .jar

--- src/businessschedule/util/Conexao.java
+++ src/businessschedule/util/Conexao.java
@@ -7,7 +7,9 @@ import java.sql.Connection;
 public class Conexao {
     public Connection getConexao() {
         try {
-            return DriverManager.getConnection("jdbc:sqlite:.\\db\\business_schedule.db");
+            String path = "db/business_schedule.db";
+            //String path = "lib/business_schedule.db";
+            return DriverManager.getConnection("jdbc:sqlite:" + path);
         } catch (SQLException erro) {
             throw new RuntimeException(erro);
         }
