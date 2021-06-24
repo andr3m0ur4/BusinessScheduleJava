37f64a8949b9f3fcf89f29fcdbb3f6bdcf601218
André Moura <andre.benedicto@etec.sp.gov.br>
2021-06-23 21:39:30 -0300

    descomentar login

--- src/businessschedule/BusinessSchedule.java
+++ src/businessschedule/BusinessSchedule.java
@@ -1,15 +1,12 @@
 package businessschedule;
 
-import businessschedule.apresentacao.FrmHome;
 import businessschedule.apresentacao.FrmLogin;
 
 public class BusinessSchedule {
     public static boolean usuario = true;
     
     public static void main(String[] args) {
-        //FrmLogin login = new FrmLogin();
-        //login.setVisible(true);
-        
-        new FrmHome().setVisible(true);
+        FrmLogin login = new FrmLogin();
+        login.setVisible(true);
     }
 }
